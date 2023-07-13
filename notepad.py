4538cff7a77a7139f1930d8b667b456f39ed1985
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-07-13 16:37:44 -0400

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -1,42 +1,48 @@
 BATCH_SIZE = 250
-num_batches = len(document_ids) // BATCH_SIZE
+num_batches = len(unique_ids) // BATCH_SIZE
 
-# Download first batch of documents and train initial model
-initial_ids = document_ids[0:BATCH_SIZE]
-token = get_access_token(username, password)
-try:
-    print(f"Started processing batch 1/{num_batches}")    
-    initial_docs = [get_document_text(document_id, token) for document_id in document_ids[0:BATCH_SIZE]]
-    model = Top2Vec(documents=initial_docs, document_ids=initial_ids, speed="learn", workers=8, ngram_vocab=True)
-    print(f"Finished processing batch 1/{num_batches}")
-except Exception as e:
-    print(e)
+document_ids = []
+document_names = []
+document_contents = []
 
-# Loop over remaining batches, downloaded documents and add them to the model
-for i in range(1, num_batches):
+def process_batch(batch_ids, batch_num):
+    token = get_access_token(username, password)
+    logging.info(f"Started processing batch {batch_num}/{num_batches}")
+
+    try:
+        batch_names = [get_document_name(document_id, token) for document_id in batch_ids]
+        batch_contents = [get_document_text(document_id, token) for document_id in batch_ids]
+        
+        document_ids.append(batch_ids)
+        document_names.append(batch_names)
+        document_contents.append(batch_contents)
+    except Exception as e:
+        logging.error(f"Exception occurred while downloading batch {batch_num}: {e}")
+
+threads = []
+for i in range(num_batches):
     start = i * BATCH_SIZE
     end = (i + 1) * BATCH_SIZE
-    batch_ids = document_ids[start:end]
+    batch_ids = unique_ids[start:end]
+
+    t = threading.Thread(target=process_batch, args=(batch_ids, i + 1))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+remaining_ids = unique_ids[num_batches * BATCH_SIZE:]
+if remaining_ids:
+    logging.info("Starting processing remaining documents")
     token = get_access_token(username, password)
     try:
-        print(f"Started processing batch {i + 1}/{num_batches}")    
-        batch_documents = [get_document_text(document_id, token) for document_id in batch_ids]
-        model.add_documents(documents=batch_documents, doc_ids=batch_ids)
-        print(f"Finished processing batch {i + 1}/{num_batches}")
+        remaining_names = [get_document_name(remaining_id, token) for remaining_id in remaining_ids]
+        remaining_contents = [get_document_text(remaining_id, token) for remaining_id in remaining_ids]
+        
+        document_ids.append(remaining_ids)
+        document_names.append(remaining_names)
+        document_contents.append(remaining_contents)
     except Exception as e:
-        print(e)
-
-# If there are any remaining documents, download them and add them to the model
-if len(document_ids) % BATCH_SIZE != 0:
-    remaining_ids = document_ids[num_batches * BATCH_SIZE]
-    token = get_access_token(username, password)
-    try:
-        print(f"Started processing remaining documents")
-        remaining_documents = [get_document_text(document_id) for document_id in remaining_ids]
-        model.add_documents(documents=remaining_documents, doc_ids=remaining_ids)
-        print("Finished processing all remaining documents")
-    except Exception as e:
-        print(e)
-
-# Save trained model
-model.save("contracts_model_doc2vec")
+        logging.error(f"Exception occurred while downloading remaining documents: {e}")
+    logging.info("Finished processing all documents")

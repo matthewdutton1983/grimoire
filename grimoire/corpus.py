24cdc541966001ea0cbcbaf46857d439e02bdffe
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-07-17 20:57:51 -0400

    Created separate implementation for DoclinkConnector

--- grimoire/corpus.py
+++ grimoire/corpus.py
@@ -8,25 +8,21 @@ from datetime import datetime
 
 # Import third-party libraries
 import pandas as pd
-import requests
-from requests.exceptions import RequestException
-from retry import retry
 
 # Import project code
-from grimoire.config import CLIENT_ID, IDA_URL, RESOURCE, DOCLINK_METADATA_URL, DOCLINK_TEXT_URL
-from grimoire.dictionary import Dictionary
-from grimoire.document import Document
+from grimoire.connectors import DoclinkConnector
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Corpus:
-    def __init__(self):
+    def __init__(self, connector: DoclinkConnector):
         self.id = uuid.uuid4()
         self.created_date = datetime.now()
         self.documents = []
         self.df = pd.DataFrame()
+        self.connector = connector
         
         logging.info(f"Created new Corpus instance with ID: {self.id} at {self.created_date}")
 
@@ -37,60 +33,6 @@ class Corpus:
             "created_date": self.created_date.isoformat(),
             "num_documents": len(self.df)
         }
-    
-    @retry(RequestException, tries=3, delay=2, backoff=2)
-    def _get_access_token(self, domain, username, password):
-            url = IDA_URL
-            payload = dict(
-                client_id = CLIENT_ID,
-                grant_type = "password",
-                username = domain + "\\" + username,
-                password = password,
-                resource = RESOURCE
-            )
-            response = requests.post(url, payload)
-            response.raise_for_status()
-            token = response.json()["access_token"]
-            return token
-    
-    @retry(RequestException, tries=3, delay=2, backoff=2)
-    def _get_document_metadata(self, unique_id, token):
-        url = f"{DOCLINK_METADATA_URL}".format(unique_id)
-        payload = {}
-        headers = {
-            "Accept": "application/json",
-            "Authorization": "Bearer" + token
-        }
-        response = requests.get(url=url, headers=headers, data=payload)
-        response.raise_for_status()
-        return response.json()
-    
-    @retry(RequestException, tries=3, delay=2, backoff=2)
-    def _get_document_text(self, unique_id, token):
-        url = f"{DOCLINK_TEXT_URL}".format(unique_id)
-        payload = {}
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer" + token
-        }
-        response = requests.get(url=url, headers=headers, data=payload)
-        response.raise_for_status()
-        return response.text
-    
-    def _process_batch(self, batch_ids, batch_num, num_batches, domain, username, password):
-            token = self._get_access_token(domain, username, password)
-            logging.info(f"Started processing batch {batch_num}/{num_batches}")
-            
-            batch_metadata = []
-            batch_contents = []
-
-            try:
-                batch_metadata = [self._get_document_metadata(document_id, token) for document_id in batch_ids]
-                batch_contents = [self._get_document_text(document_id, token) for document_id in batch_ids]
-            except Exception as e:
-                logging.error(f"Exception occurred while downloading batch {batch_num}: {e}")
-
-            return batch_ids, batch_metadata, batch_contents
 
     def add_documents(self, document_ids, domain, username, password, batch_size):
         BATCH_SIZE = batch_size
@@ -106,7 +48,7 @@ class Corpus:
                 start = i * BATCH_SIZE
                 end = (i + 1) * BATCH_SIZE
                 batch_ids = document_ids[start:end]
-                futures.append(executor.submit(self._process_batch, batch_ids, i + 1, num_batches, domain, username, password))
+                futures.append(executor.submit(self.connector.process_batch, batch_ids, i + 1, num_batches, domain, username, password))
 
             for future in concurrent.futures.as_completed(futures):
                 batch_ids, batch_metadata, batch_contents = future.result()
@@ -119,8 +61,8 @@ class Corpus:
             logging.info("Started processing remaining documents")
             token = self._get_access_token(domain, username, password)
             try:
-                remaining_metadata = [self._get_document_metadata(remaining_id, token) for remaining_id in remaining_ids]
-                remaining_content = [self._get_document_text(remaining_id, token) for remaining_id in remaining_ids]
+                remaining_metadata = [self.connector._get_document_metadata(remaining_id, token) for remaining_id in remaining_ids]
+                remaining_content = [self.connector._get_document_text(remaining_id, token) for remaining_id in remaining_ids]
 
                 all_document_ids.extend(remaining_ids)
                 all_document_metadata.extend(remaining_metadata)

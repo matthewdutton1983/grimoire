2ca971de4be24b96d05869c63a67c26fb4e530d9
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-07-18 16:48:44 -0400

    Update document.py

--- grimoire/document.py
+++ grimoire/document.py
@@ -1,13 +1,13 @@
 class Document:
-    def __init__(self, id, text, taxonomy):
+    def __init__(self, id, text, attributes):
         self.id = id
         self.text = text
-        self.taxonomy = taxonomy
+        self.attributes = attributes
 
     @property
     def view_info(self):
         return {
             "id": str(self.id),
             "text": self.text,
-            "taxonomy": self.taxonomy
+            "attributes": self.attributes
         }

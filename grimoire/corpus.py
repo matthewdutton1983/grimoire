8220ac6b3361101ea40cfb7afeb9635bf7ecf63c
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-07-18 14:40:03 -0400

    Update corpus.py

--- grimoire/corpus.py
+++ grimoire/corpus.py
@@ -6,22 +6,20 @@ import random
 import uuid
 from datetime import datetime
 
-# Import third-party libraries
-import pandas as pd
-
 # Import project code
 from grimoire.connectors import DoclinkConnector
+from grimoire.document import Document
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Corpus:
-    def __init__(self, connector: DoclinkConnector):
+    def __init__(self, connector = DoclinkConnector):
         self.id = uuid.uuid4()
         self.created_date = datetime.now()
         self.documents = []
-        self.df = pd.DataFrame()
+        self.id_to_index = {}
         self.connector = connector
         
         logging.info(f"Created new Corpus instance with ID: {self.id} at {self.created_date}")
@@ -31,19 +29,18 @@ class Corpus:
         return {
             "id": str(self.id),
             "created_date": self.created_date.isoformat(),
-            "num_documents": len(self.df)
+            "num_documents": len(self.documents)
         }
 
-    def add_documents(self, document_ids, domain, username, password, batch_size):
-        BATCH_SIZE = batch_size
+    def add_documents(self, document_ids, domain, username, password):
+        BATCH_SIZE = max(1, len(document_ids) // 100)
         num_batches = len(document_ids) // BATCH_SIZE
 
-        all_document_ids = []
-        all_document_metadata = []
-        all_document_contents = []
+        all_documents = []
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = []
+        
             for i in range(num_batches):
                 start = i * BATCH_SIZE
                 end = (i + 1) * BATCH_SIZE
@@ -52,39 +49,47 @@ class Corpus:
 
             for future in concurrent.futures.as_completed(futures):
                 batch_ids, batch_metadata, batch_contents = future.result()
-                all_document_ids.extend(batch_ids)
-                all_document_metadata.extend(batch_metadata)
-                all_document_contents.extend(batch_contents)
+
+                for i in range(len(batch_ids)):
+                    all_documents.append(Document(batch_ids[i], batch_contents[i], batch_metadata[i]))
 
         remaining_ids = document_ids[num_batches * BATCH_SIZE:]
+
         if remaining_ids:
             logging.info("Started processing remaining documents")
-            token = self._get_access_token(domain, username, password)
+            token = self.connector.get_access_token(domain, username, password)
+            
             try:
-                remaining_metadata = [self.connector._get_document_metadata(remaining_id, token) for remaining_id in remaining_ids]
-                remaining_content = [self.connector._get_document_text(remaining_id, token) for remaining_id in remaining_ids]
+                remaining_metadata = [self.connector.get_document_metadata(remaining_id, token) for remaining_id in remaining_ids]
+                remaining_content = [self.connector.get_document_text(remaining_id, token) for remaining_id in remaining_ids]
 
-                all_document_ids.extend(remaining_ids)
-                all_document_metadata.extend(remaining_metadata)
-                all_document_contents.extend(remaining_content)
+                for i in range(len(remaining_ids)):
+                    all_documents.append(Document(remaining_ids[i], remaining_content[i], remaining_metadata[i]))
             except Exception as e:
                 logging.error(f"Error occurred while downloading remaining documents")
 
         logging.info("All documents have been downloaded and added to the corpus")
 
-        self.df = pd.DataFrame({
-            "DOCUMENT_ID": all_document_ids,
-            "DOCUMENT_METADATA": all_document_metadata,
-            "DOCUMENT_CONTENT": all_document_contents
-        })
+        self.documents.extend(all_documents)
 
-        return self.df
+        for i, doc in enumerate(all_documents):
+            self.id_to_index[doc.id] = len(self.documents) - len(all_documents) + i
     
+    def get_document_by_id(self, document_id):
+        if document_id in self.id_to_index:
+            return self.documents[self.id_to_index[document_id]]
+        else:
+            logging.error(f"No document found with ID: {document_id}")
+            return None
+        
     def remove_documents(self, document_ids):
-        self.df = self.df[~self.df["DOCUMENT_ID"].isin(document_ids)]
-        logging.info(f"Successfully removed documents from the corpus: {document_ids}")
-        return self.df
+        self.documents = [doc for doc in self.documents if doc.id not in document_ids]
+        self.id_to_index = {id: i for i, id in enumerate(doc.id for doc in self.documents)}
+        logging.info(f"Successfully removed documents from corpus: {document_ids}")
 
+    def search_corpus(self, query):
+        return [doc for doc in self.documents if query.lower() in doc.content.lower()]
+    
     def random_sample(self, n):
         return random.sample(self.documents, n)
 
@@ -96,4 +101,4 @@ class Corpus:
     def load_corpus(filename):
         with open(filename, "rb") as f:
             return pickle.load(f)
-    
+        

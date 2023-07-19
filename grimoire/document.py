e861f303d42f39bb13a65cbfdef07d31b89701bf
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-07-19 19:31:04 -0400

    Update document.py

--- grimoire/document.py
+++ grimoire/document.py
@@ -1,35 +1,23 @@
 # Import native libraries
-from typing import Any, Dict
+from datetime import datetime
+
+# Import project code
+from grimoire.dictionary import Dictionary
 
 
 class Document:
-    """
-    This class represents a Document in the Corpus. It contains the id, text, and attributes of the document.
-    """
-
-    def __init__(self, id: str, text: str, attributes: Dict[str, Any]) -> None:
-        """
-        Initializes a new Document instance.
-
-        Args:
-            id (str): The id of the document.
-            text (str): The text content of the document.
-            attributes (Dict[str, Any]): The attributes of the document.
-        """
+    def __init__(self, id, text, attributes):
         self.id = id
+        self.date_added = str(datetime.now())
         self.text = text
         self.attributes = attributes
-
+        
     @property
-    def view_info(self) -> Dict[str, Any]:
-        """
-        Returns the metadata of the document.
-
-        Returns:
-            Dict[str, Any]: A dictionary with the id, text, and attributes of the document.
-        """
+    def view_info(self):
         return {
             "id": str(self.id),
+            "date_added": self.date_added,
             "text": self.text,
             "attributes": self.attributes
         }
+          

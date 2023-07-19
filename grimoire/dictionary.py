8bfa98cee50a374c37a8c7c387e0abcefc7c596b
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-07-19 19:31:22 -0400

    Update dictionary.py

--- grimoire/dictionary.py
+++ grimoire/dictionary.py
@@ -1,3 +1,26 @@
+# Import native libraries
+import logging
+import uuid
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
 class Dictionary:
-    def __init__(self):
-        self.words = {}
+    def __init__(self, tokenizer):
+        self.id = uuid.uuid4()
+        self.date_created = datetime.now()
+        self.tokenizer = tokenizer
+        self.vocabulary = set()
+
+        logger.info("Created new Dictionary instance")
+
+    @property
+    def view_info(self):
+        return {
+            "id": str(self.id),
+            "date_created": self.date_created,
+            "tokenizer": self.tokenizer,
+            "vocabulary": self.vocabulary
+        }

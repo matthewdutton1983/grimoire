34243518ae3f39def2479dfe310439702d9b256f
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-07-22 13:21:42 -0400

    Update features.py

--- grimoire/nlp/features.py
+++ grimoire/nlp/features.py
@@ -1,10 +1,17 @@
+# Import native libraries
+import logging
+
 # Import third-party libraries
 import spacy
 
-model = spacy.load("/models/en_core_web_lg-3.4.0/")
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Features:
+    logger.info("Loading NLP model ... ")
+    nlp = spacy.load("/models/en_core_web_lg-3.4.0/")
+
     def __init__(self):
         self.tokens = []
         self.lemma = []
@@ -19,17 +26,22 @@ class Features:
         self.titlecase = []
         self.numeric = [] 
 
-    def extract_features(self, document):
-        self.nlp = model(document.text)
-        self.tokens = [token for token in self.nlp]
-        self.lemma = [token.lemma_ for token in self.tokens]
-        self.syntax = [token.pos_ for token in self.tokens]
-        self.tags = [token.tag_ for token in self.tokens]
-        self.dep = [token.dep_ for token in self.tokens]
-        self.shape = [token.shape_ for token in self.tokens]
-        self.alpha = [token.is_alpha for token in self.tokens]
-        self.stopword = [token.is_stop for token in self.tokens]
-        self.lowercase = [str(token).islower() for token in self.tokens]
-        self.uppercase = [str(token).isupper() for token in self.tokens]
-        self.titlecase = [str(token).istitle() for token in self.tokens]
-        self.numeric = [str(token).isnumeric() for token in self.tokens]
+
+    @classmethod
+    def extract_features(cls, text):
+        features = cls()
+        doc = cls.nlp(text)
+        logger.info("Extracting features ...")
+        features.tokens = [token for token in doc]
+        features.lemma = [token.lemma_ for token in features.tokens]
+        features.syntax = [token.pos_ for token in features.tokens]
+        features.tags = [token.tag_ for token in features.tokens]
+        features.dep = [token.dep_ for token in features.tokens]
+        features.shape = [token.shape_ for token in features.tokens]
+        features.alpha = [token.is_alpha for token in features.tokens]
+        features.stopword = [token.is_stop for token in features.tokens]
+        features.lowercase = [str(token).islower() for token in features.tokens]
+        features.uppercase = [str(token).isupper() for token in features.tokens]
+        features.titlecase = [str(token).istitle() for token in features.tokens]
+        features.numeric = [str(token).isnumeric() for token in features.tokens]
+        return features

df18fb4542fc10dc2313bd03a54119b2eb169e78
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-07-22 17:31:13 -0400

    Update features.py

--- grimoire/nlp/features.py
+++ grimoire/nlp/features.py
@@ -2,6 +2,7 @@
 import logging
 
 # Import third-party libraries
+import pandas as pd
 import spacy
 
 logger = logging.getLogger(__name__)
@@ -13,6 +14,7 @@ class Features:
     nlp = spacy.load("/models/en_core_web_lg-3.4.0/")
 
     def __init__(self):
+        self.noun_chunks = []
         self.tokens = []
         self.lemma = []
         self.syntax = []
@@ -24,14 +26,18 @@ class Features:
         self.lowercase = []
         self.uppercase = []
         self.titlecase = []
-        self.numeric = [] 
+        self.numeric = []
+        self.entities = [] 
 
 
     @classmethod
     def extract_features(cls, text):
         features = cls()
         doc = cls.nlp(text)
+        
         logger.info("Extracting features ...")
+        
+        features.noun_chunks = list(doc.noun_chunks)
         features.tokens = [token for token in doc]
         features.lemma = [token.lemma_ for token in features.tokens]
         features.syntax = [token.pos_ for token in features.tokens]
@@ -44,4 +50,19 @@ class Features:
         features.uppercase = [str(token).isupper() for token in features.tokens]
         features.titlecase = [str(token).istitle() for token in features.tokens]
         features.numeric = [str(token).isnumeric() for token in features.tokens]
+        
+        for ent in doc.ents:
+            features.entities.append((ent.text, ent.start_char, ent.end_char, ent.label_))
+        
         return features
+    
+    @classmethod
+    def summarize_features(cls):
+        features = cls()
+        columns = ["TOKEN", "LEMMA", "POS", "TAG", "DEP", "SHAPE", "ALPHA", "STOP", "LOWER", "UPPER", "TITLE", "NUMERIC"]
+        data = zip(features.tokens, features.lemma, features.syntax, features.tags, features.dep,
+            features.shape, features.alpha, features.stopword, features.lowercase,
+            features.uppercase, features.titlecase, features.numeric
+        )
+        df = pd.DataFrame(data, columns=columns)
+        return df
